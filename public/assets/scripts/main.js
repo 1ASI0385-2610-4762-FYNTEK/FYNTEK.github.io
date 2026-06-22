26eaf4a290e42eb52a3fff3d86e3eb83efa30a9c
AleDusty <u20221g231@upc.edu.pe>
2026-06-22 15:10:34 -0500

    feat(assets): initialize Vitalis scripts with domcontentloaded listener and trial alert

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Ecosistema Zen de VITALIS inicializado de forma correcta.");
+});
+
+
+function initTrial() {
+    alert("Te damos la bienvenida a Vitalis. Reduciendo tu carga cognitiva en 3, 2, 1...");
+}
